0212e6f2188889098a21ec3993ffda3109de3ae4
KÉSIA PRISCILLA DA SILVA BONFIM <kesia.bonfim@edu.unimontes.br>
2026-06-10 16:23:20 -0300

    versão com arquivo vaio da primeira model e  view

---
+++ resources/views/home.php
@@ -0,0 +1,3 @@
+//area pública (onde clientes podem ver catálogos de produto, etc)
+//cabecaçalho (com login, etc)
+//rodape (com contatos, etc)
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+//nova rota criada
+Route::get('/home', function () {
+    return view('home');
+})->middleware(['auth'])->name('home');
