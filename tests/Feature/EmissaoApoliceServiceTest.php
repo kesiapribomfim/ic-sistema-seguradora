0350d7355f7b340fe5657b87168ea74f6a895d60
Kesia Bomfim <kesia.prisil@gmail.com>
2026-09-13 15:10:43 -0300

    Criação da Classe Test para EmissaoApoliceService

---
+++ tests/Feature/EmissaoApoliceServiceTest.php
@@ -0,0 +1,14 @@
+<?php
+
+use App\Services\CalculadoraPremioService;
+use App\Models\Produto;
+use App\Models\Segurado;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+
+/**
+ * Classe de teste destinada a Service CalculadoraPremioService
+ */
+
+uses(RefreshDatabase::class);
